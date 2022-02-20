a9042995f58f46ffeef5c53a834cf35f36566a62
wasuken <wasuken1@gmail.com>
2022-02-20 21:48:14 +0900

    impl sort.

--- components/CSVList.jsx
+++ components/CSVList.jsx
@@ -1,9 +1,11 @@
 import { useQuery } from "react-query";
 import { useCSVContext } from "../context/csv";
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Papa from "papaparse";
 import Modal from "react-modal";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import styles from "../styles/CSVList.module.css";
 
 const customStyles = {
   content: {
@@ -19,16 +21,66 @@ const customStyles = {
 
 Modal.setAppElement("#edit-modal");
 
+const initSortInfoState = {
+  key: "",
+  isSort: false,
+  order: true,
+};
+
 function CSVList() {
   const [modalIsOpen, setIsOpen] = useState(-1);
   const [editRow, setEditRow] = useState([]);
+  const [sortInfo, setSortInfo] = useState(initSortInfoState);
+  const { csv, setcsv, page } = useCSVContext();
+
+  useEffect(() => {
+    if (csv.length > 0) {
+      let csvv = [...csv.slice(1)];
+      let ii = -1;
+      csv[0].forEach((v, i) => {
+        if (v === sortInfo.key) {
+          ii = i;
+        }
+      });
+      csvv.sort((a, b) => {
+        if (a[ii] > b[ii]) {
+          return sortInfo.order ? -1 : 1;
+        } else if (a[ii] < b[ii]) {
+          return sortInfo.order ? 1 : -1;
+        } else {
+          return 0;
+        }
+      });
+      setcsv([csv[0], ...csvv]);
+    }
+  }, [sortInfo]);
+
+  function updateSortInfo(k) {
+    let info = { ...sortInfo };
+    if (info.isSort) {
+      // すでにソート済の処理
+      if (info.key === k) {
+        // 切り替え
+        info.order = !info.order;
+      } else {
+        // key変更
+        info.key = k;
+      }
+    } else {
+      // 未設定
+      info.isSort = true;
+      info.key = k;
+    }
+
+    setSortInfo(info);
+  }
 
   function openModal(i) {
     setIsOpen(i);
     setEditRow([...csv[i]]);
   }
   function handleEditRowItem(i, e) {
-	const v = e.target.value;
+    const v = e.target.value;
     let newrow = [...editRow];
     newrow[i] = v;
     setEditRow([...newrow]);
@@ -50,16 +102,21 @@ function CSVList() {
     setcsv(newcsv);
     closeModal();
   }
-  function handleDelete(){
-	let newcsv = [...csv];
-	newcsv.splice(modalIsOpen, 1);
-	setcsv(newcsv);
-	closeModal();
+  function handleDelete() {
+    let newcsv = [...csv];
+    newcsv.splice(modalIsOpen, 1);
+    setcsv(newcsv);
+    closeModal();
   }
-  const { csv, setcsv, page } = useCSVContext();
   if (csv.length <= 0) return <p>empty.</p>;
   return (
     <div className="" style={{ width: "3000px" }}>
+      <button
+        className="btn btn-primary"
+        onClick={() => setSortInfo(initSortInfoState)}
+      >
+        SortClear
+      </button>
       <table
         className="table table-dark align-middle table-hover"
         style={{ maxWidth: "100%" }}
@@ -67,8 +124,19 @@ function CSVList() {
         <thead>
           <tr>
             {csv[0].map((h, i) => (
-              <th scope="col w-25" key={i}>
-                {h}
+              <th
+                scope="col w-25 ptr"
+                key={i}
+                onClick={() => updateSortInfo(h)}
+              >
+                <div className={styles.ptr}>
+                  {h}
+                  {sortInfo.isSort && sortInfo.key === h
+                    ? sortInfo.order
+                      ? "↑"
+                      : "↓"
+                    : ""}
+                </div>
               </th>
             ))}
           </tr>
@@ -129,7 +197,7 @@ function CSVList() {
           <button className="btn btn-secondary ml-1" onClick={closeModal}>
             Cancel
           </button>
-		  <button className="btn btn-danger ml-1" onClick={handleDelete}>
+          <button className="btn btn-danger ml-1" onClick={handleDelete}>
             Remove
           </button>
         </div>
